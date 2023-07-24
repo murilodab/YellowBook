bc8fdb7ac7f1a0f89046bd9877ff165e122d4d62
Murilo Barbosa <muriloab96@gmail.com>
2023-07-24 16:56:27 -0300

    Add project files.

---
+++ YellowBook/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace YellowBook.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
