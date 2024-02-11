cace32658becb92f9d9a985585d535bc53a79941
Murilo Barbosa <muriloab96@gmail.com>
2024-02-11 19:42:39 -0300

    added query splitting behavior

--- Program.cs
+++ Program.cs
@@ -17,7 +17,8 @@ var builder = WebApplication.CreateBuilder(args);
 var connectionString = ConnectionHelper.GetConnectionString(builder.Configuration);
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseNpgsql(connectionString));
+    options.UseNpgsql(connectionString,
+    o => o.UseQuerySplittingBehavior(QuerySplittingBehavior.SplitQuery)));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<AppUser>(options => options.SignIn.RequireConfirmedAccount = true)
